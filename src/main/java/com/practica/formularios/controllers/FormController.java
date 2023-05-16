fbf670e729e7ee230047371c77b459224c65c4b9
jjortiz016 <jjortiz016@misena.edu.co>
2023-05-15 22:04:29 -0500

    se creo la clase usuario y se retorno el usuario como objeto al resultado.html

--- src/main/java/com/practica/formularios/controllers/FormController.java
+++ src/main/java/com/practica/formularios/controllers/FormController.java
@@ -1,5 +1,6 @@
 package com.practica.formularios.controllers;
 
+import com.practica.formularios.models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,10 +21,14 @@ public class FormController {
                            @RequestParam String password,
                            @RequestParam String email,
                            Model model){
+
+        Usuario usuario = new Usuario();
+        usuario.setUsername(username);
+        usuario.setPassword(password);
+        usuario.setEmail(email);
+
         model.addAttribute("titulo", "Resultado form");
-        model.addAttribute("username", username);
-        model.addAttribute("password", password);
-        model.addAttribute("email", email);
+        model.addAttribute("usuario", usuario);
         return "resultado";
     }
 
