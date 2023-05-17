37b1f3bc5363274656494e48d2acd2f976c98c8c
jjortiz016 <jjortiz016@misena.edu.co>
2023-05-16 22:39:57 -0500

    Añadiendo mensaje de errores al formulario

--- src/main/java/com/practica/formularios/controllers/FormController.java
+++ src/main/java/com/practica/formularios/controllers/FormController.java
@@ -1,10 +1,16 @@
 package com.practica.formularios.controllers;
 
 import com.practica.formularios.models.Usuario;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
@@ -12,7 +18,9 @@ public class FormController {
 
     @GetMapping("/form")
     public String form(Model model){
+        Usuario usuario= new Usuario();
         model.addAttribute("titulo", "Formulario usuarios");
+        model.addAttribute("usuario", usuario);
         return "form";
     }
 
@@ -34,10 +42,19 @@ public class FormController {
 
     //OPTIMIZANDO PARA RECIBIR COMO OBJETO
       @PostMapping("/form")
-    public String procesar(Usuario usuario,
+    public String procesar(@Valid Usuario usuario, BindingResult result,
                            Model model){
+          model.addAttribute("titulo", "Resultado form");
+       if(result.hasErrors()){
+           Map<String, String> errores = new HashMap<>(); // Map lista asociativa basada en nombre y valor
+           result.getFieldErrors().forEach(err ->{  //getFieldErrors() es una lista
+               errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+           });
+           model.addAttribute("error", errores);
+            return "form";
+       }
 
-        model.addAttribute("titulo", "Resultado form");
+
         model.addAttribute("usuario", usuario);
         return "resultado";
     }
