cc5b5b2434e3f4ab0b81870daebfc20c5f163ddd
jjortiz016 <jjortiz016@misena.edu.co>
2023-07-19 16:14:29 -0500

    se modifico archivvo messages.properties para personalizar los mensajes de validación, se agrego campo name, la stName, id a la clase usuario y se modifico el form2, se provaron las validaciones, funciona correctamente

--- src/main/java/com/practica/formularios/models/Usuario.java
+++ src/main/java/com/practica/formularios/models/Usuario.java
@@ -1,14 +1,49 @@
 package com.practica.formularios.models;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 public class Usuario {
+    private String id;
+
     @NotEmpty
+    private String name;
+    @NotEmpty
+    private String lastName;
+
+    @NotEmpty
+    @Size(min=3,max=8)
     private String username;
     @NotEmpty
     private String password;
     @NotEmpty
+    @Email
     private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     public String getUsername() {
         return username;
